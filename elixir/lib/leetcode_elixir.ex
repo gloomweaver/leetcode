fce53ff6324c4feb70dc1eabd50d2aea66e90fd2
Kirill Khramkov <hramkir@gmail.com>
2023-02-15 03:48:57 +0400

    elixir solution for problem 1

---
+++ elixir/lib/leetcode_elixir.ex
@@ -0,0 +1,2 @@
+defmodule LeetcodeElixir do
+end
---
+++ elixir/test/leetcode_elixir_test.exs
@@ -0,0 +1,8 @@
+defmodule LeetcodeElixirTest do
+  use ExUnit.Case
+  doctest LeetcodeElixir
+
+  test "greets the world" do
+    assert LeetcodeElixir.hello() == :world
+  end
+end
