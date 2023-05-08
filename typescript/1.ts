d6f4a498371d0ca23fd5e592359db2ef992bdeff
Kirill Khramkov <hramkir@gmail.com>
2023-05-08 12:47:58 +0400

    typescript: add problem 1 solution

---
+++ typescript/1.ts
@@ -0,0 +1,15 @@
+function twoSum(nums: number[], target: number): number[] {
+  const elementExists = new Map<number, number>();
+  for (let curIndex = 0; curIndex < nums.length; curIndex++) {
+    const cur = nums[curIndex];
+    const pair = elementExists.get(target - cur);
+    if (pair !== undefined) {
+      return [pair, curIndex];
+    }
+    if (!elementExists.get(cur)) {
+      elementExists.set(cur, curIndex);
+    }
+  }
+  return [NaN, NaN];
+}
+
