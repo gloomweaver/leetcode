77421699685a2470a04d66b00f0191c55569b3f2
Kirill Khramkov <hramkir@gmail.com>
2023-06-01 06:56:01 +0400

    typescript: problem 387

---
+++ typescript/src/387.ts
@@ -0,0 +1,12 @@
+function firstUniqChar(s: string): number {
+  const map = new Map<string, number>();
+  for (const char of s) {
+    map.set(char, (map.get(char) || 0) + 1);
+  }
+  for (let i = 0; i < s.length; i++) {
+    if (map.get(s[i]) === 1) {
+      return i;
+    }
+  }
+  return -1;
+}
