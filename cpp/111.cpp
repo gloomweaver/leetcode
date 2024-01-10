4e26da886079886fd1bf0bfb3863dd89cc4e9570
Kirill Khramkov <hramkir@gmail.com>
2024-01-10 23:40:19 +0400

    cpp: problem 111 solved

---
+++ cpp/111.cpp
@@ -0,0 +1,26 @@
+#include <algorithm>
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    int minDepth(TreeNode *root)
+    {
+        if (!root)
+            return 0;
+
+        int left = minDepth(root->left),
+            right = minDepth(root->right);
+
+        return (left && right ? std::min(left, right) : std::max(left, right)) + 1;
+    }
+};
