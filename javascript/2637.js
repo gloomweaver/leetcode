7f60eec6158d938d12af0024500f124ee9b18cff
Kirill Khramkov <hramkir@gmail.com>
2024-05-04 22:14:21 +0400

    Javascript 2637

---
+++ javascript/2637.js
@@ -0,0 +1,20 @@
+/**
+ * @param {Function} fn
+ * @param {number} t
+ * @return {Function}
+ */
+function timeLimit(fn, t) {
+  return async function (...args) {
+    return new Promise(async (resolve, reject) => {
+      setTimeout(() => reject("Time Limit Exceeded"), t);
+      fn(...args)
+        .then(resolve)
+        .catch(reject);
+    });
+  };
+}
+
+/**
+ * const limited = timeLimit((t) => new Promise(res => setTimeout(res, t)), 100);
+ * limited(150).catch(console.log) // "Time Limit Exceeded" at t=100ms
+ */
