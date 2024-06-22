30a0ed5fa5055be1bb446b7f18e3b3d6e09aef49
Kirill Khramkov <hramkir@gmail.com>
2024-06-22 22:04:24 +0400

    Javascript 2703

---
+++ javascript/2703.js
@@ -0,0 +1,9 @@
+/**
+ * @param {...(null|boolean|number|string|Array|Object)} args
+ * @return {number}
+ */
+const argumentsLength = (...args) => args.length;
+
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
